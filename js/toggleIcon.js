108faf34cc4648665cde6e79f9163fa715d7d49b
Alexander Pletnev <pletnev.a.a@yandex.ru>
2022-12-10 22:20:29 +0100

    remove icon switch toggler, add red app icon

--- js/toggleIcon.js
+++ js/toggleIcon.js
@@ -1,3 +1,8 @@
-if (window.matchMedia('prefers-color-scheme: dark').matches) {
+// TODO: The window object isn't available inside a ServiceWorker, and thus
+// we cannot easily move from Manifest v2 to Manifest v3 yet.
+// See https://github.com/w3c/webextensions/issues/229.
+
+/* if (window.matchMedia('prefers-color-scheme: dark').matches) {
     chrome.runtime.sendMessage({ scheme: "dark" });
-}
+} */
+
