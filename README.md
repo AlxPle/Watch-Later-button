3ff482f4cc196392a70f884f8e59e10c678aec9b
Alexander Pletnev <pletnev.a.a@yandex.ru>
2022-12-12 00:34:15 +0100

    MutationObserver function for waiting element

--- README.md
+++ README.md
@@ -4,4 +4,10 @@ Pet project
 
 
 TODO: 
-1. Auto switch for dark/light browser mode
+1. Auto switch for dark/light browser mode browser app icon
+2. Auto detect dark/light Youtube mode
+3. Fast SaveTo - WatchLater
+3.1 Add opaticy to popup menu: aria-hidden="true" to focused -- add opaticy
+3.2 Disable Overlay: 
+<tp-yt-iron-overlay-backdrop style="z-index: 2201;" class="opened" opened=""></tp-yt-iron-overlay-backdrop>
+3.3 Add opaticy to popup menu with playlists: aria-hidden="true" 
--- js/saveto.js
+++ js/saveto.js
@@ -1,38 +1,58 @@
-setTimeout(() => {
-  /* Wait for bar is load */
+/* https://stackoverflow.com/questions/5525071/how-to-wait-until-an-element-exists */
+/* https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver */
+function waitForElm(selector) {
+  return new Promise((resolve) => {
+    if (document.querySelector(selector)) {
+      return resolve(document.querySelector(selector));
+    }
+
+    const observer = new MutationObserver((mutations) => {
+      if (document.querySelector(selector)) {
+        resolve(document.querySelector(selector));
+        observer.disconnect();
+      }
+    });
+
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
+  });
+}
+
+waitForElm("#actions").then((elm) => {
+  console.log('Element is ready');
   let appendItem = document.getElementById("top-level-buttons-computed");
 
-  if (appendItem) {
-    /* Create saveTo button */
-    let saveTo = document.createElement("div");
-    /* Set id to element */
-    saveTo.id = "saveToPlaylist";
-    /* Set class name to element */
-    saveTo.className = "save-to";
+  /* Create saveTo button */
+  let saveTo = document.createElement("div");
+  /* Set id to element */
+  saveTo.id = "saveToPlaylist";
+  /* Set class name to element */
+  saveTo.className = "save-to";
 
-    /* Create saveTo button in DOM */
-    appendItem.prepend(saveTo);
+  /* Create saveTo button in DOM */
+  appendItem.prepend(saveTo);
 
-    /* Find "Save" btn */
-    function searchSaveBtn() {
-      var saveBtn = document.evaluate(
-        "//yt-formatted-string[contains(., 'Save')]",
-        document,
-        null,
-        XPathResult.ANY_TYPE,
-        null
-      );
-      var thisBtn = saveBtn.iterateNext();
-      var parentBtn = thisBtn.parentElement;
-      var nextParentBtn = parentBtn.parentElement;
-      nextParentBtn.click();
-    }
-    /* Setup Click event */
-    saveTo.addEventListener("click", () => {
-      document.querySelector("#button-shape > button").click();
-      searchSaveBtn();
-    });
+  /* Find "Save" btn */
+  function searchSaveBtn() {
+    var saveBtn = document.evaluate(
+      "//yt-formatted-string[contains(., 'Save')]",
+      document,
+      null,
+      XPathResult.ANY_TYPE,
+      null
+    );
+    var thisBtn = saveBtn.iterateNext();
+    var parentBtn = thisBtn.parentElement;
+    var nextParentBtn = parentBtn.parentElement;
+    nextParentBtn.click();
   }
-}, 4000);
+  /* Setup Click event */
+  saveTo.addEventListener("click", () => {
+    document.querySelector("#button-shape > button").click();
+    searchSaveBtn();
+  });
+});
 
 /* style-scope ytd-menu-service-item-renderer */
