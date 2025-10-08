02783120520338bc939cc4a2cfad68664f16ebcb
2thecrow <pletnev1990.a.a@gmail.com>
2025-10-08 22:04:55 +0200

    delete Firefox version

--- firefox/js/toggleIcon.js
+++
@@ -1,8 +0,0 @@
-// TODO: The window object isn't available inside a ServiceWorker, and thus
-// we cannot easily move from Manifest v2 to Manifest v3 yet.
-// See https://github.com/w3c/webextensions/issues/229.
-
-/* if (window.matchMedia('prefers-color-scheme: dark').matches) {
-    chrome.runtime.sendMessage({ scheme: "dark" });
-} */
-
