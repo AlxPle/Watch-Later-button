96a6cd3e3c3907220340c857c3ddc6b72561e5e5
2thecrow <pletnev1990.a.a@gmail.com>
2025-10-09 13:03:11 +0200

    add i18n support

---
+++ chrome/popup/popup.js
@@ -0,0 +1,4 @@
+document.getElementById('extension-name').textContent = chrome.i18n.getMessage('extensionName');
+document.getElementById('issues').textContent = chrome.i18n.getMessage('issues');
+document.getElementById('feedback').textContent = chrome.i18n.getMessage('feedback');
+document.getElementById('donate').textContent = chrome.i18n.getMessage('donate');
