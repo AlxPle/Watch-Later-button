9334dd2a649efe3fc30d8c2ec6c90f11cf6857d3
AlxPle <pletnev1990.a.a@gmail.com>
2026-03-30 16:28:08 +0200

    redesign popup

--- chrome/popup/popup.js
+++ chrome/popup/popup.js
@@ -2,3 +2,9 @@ document.getElementById('extension-name').textContent = chrome.i18n.getMessage('
 document.getElementById('issues').textContent = chrome.i18n.getMessage('issues');
 document.getElementById('feedback').textContent = chrome.i18n.getMessage('feedback');
 document.getElementById('donate').textContent = chrome.i18n.getMessage('donate');
+document.getElementById('contact').textContent = chrome.i18n.getMessage('contact');
+document.getElementById('website').textContent = chrome.i18n.getMessage('website');
+document.getElementById('whats-new').textContent = chrome.i18n.getMessage('whatsNew');
+
+const manifest = chrome.runtime.getManifest();
+document.getElementById('ext-version').textContent = `v${manifest.version}`;
